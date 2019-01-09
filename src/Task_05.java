7772cff3bc7751563536048c828fc488e373f51a
AsyaDemintseva <asdemya@mail.com>
2019-01-10 00:52:44 +0300

    Initial commit

---
+++ src/Task_05.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task_05 {
+
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        int b = Integer.parseInt(a, 2);
+        System.out.println(b);
+    }
+}
